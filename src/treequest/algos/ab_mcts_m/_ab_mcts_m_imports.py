ac10812e1b4d5d4df5cded9f49a957fd2edf200e
takkyu2 <mk940se@gmail.com>
2025-08-01 02:02:11 +0900

    add workaround

--- src/treequest/algos/ab_mcts_m/_ab_mcts_m_imports.py
+++ src/treequest/algos/ab_mcts_m/_ab_mcts_m_imports.py
@@ -2,6 +2,15 @@ from treequest.imports import try_import
 
 with try_import() as _import:
     import jax
+    from packaging.version import Version
+
+    # TODO: Remove this hotfix after numpyro fixes incompatibility with jax>=0.7
+    # https://github.com/pyro-ppl/numpyro/issues/2051
+    if jax.__version__ >= Version("0.7.0"):
+        import jax.experimental.pjit as _pjit
+        from jax.extend.core.primitives import jit_p
+
+        _pjit.pjit_p = jit_p
     import numpy as np
     import numpyro  # type: ignore
     import pandas as pd  # type: ignore
